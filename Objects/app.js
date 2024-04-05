9908ec5a15e0a86ce6a31c70d827ed773d0e2a68
Faisalirfan25 <faisalirfan619@gmail.com>
2024-04-05 23:16:23 +0500

    Create app.js

---
+++ Objects/app.js
@@ -0,0 +1,15 @@
+const person = 
+{
+    firstname : 'faisal',
+    lastname : 'irfan'
+}
+
+const product = {
+    name :"GummyBears",
+    inStock : true,
+    price: 1.99,
+    flavors: ["grape","apple","cherry"]
+    
+}
+product.firstname
+console.log(product)
